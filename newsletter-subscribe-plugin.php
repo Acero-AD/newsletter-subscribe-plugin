5190fe94f8b718adf9fdd3cb63afa234cecc222b
Diego Acero <diegoaceroarguelles@gmail.com>
2024-10-15 21:03:04 +0200

    Create configuration page

--- newsletter-subscribe-plugin.php
+++ newsletter-subscribe-plugin.php
@@ -29,3 +29,52 @@ function subscribe_form() {
 }
 
 add_shortcode('newsletter-subscribe-form', 'subscribe_form');
+
+add_action('admin_menu', 'newsletter_subscribe_plugin_menu');
+
+function newsletter_subscribe_plugin_menu() {
+  add_menu_page(
+    'Newsletter Plugins Settings',
+    'Newsletter Settings',
+    'manage_options',
+    'newsletter-subscribe-settings',
+    'newsletter_subscribe_plugin_page'
+  );
+}
+
+function newsletter_subscribe_plugin_page() {
+  ?>
+    <div class="wrap">
+        <h1>Configuración de Suscripción a Beehiiv</h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('newsletter-subscribe-settings-group');
+            do_settings_sections('newsletter-subscribe-settings-group');
+            ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">URL de la API de Beehiiv</th>
+                    <td><input type="text" name="provider_api_url" value="<?php echo esc_attr(get_option('provider_api_url')); ?>" style="width: 100%;"/></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Clave API de Beehiiv</th>
+                    <td><input type="text" name="provider_api_key" value="<?php echo esc_attr(get_option('provider_api_key')); ?>" style="width: 100%;"/></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">CSS Personalizado</th>
+                    <td><textarea name="form_custom_css" style="width: 100%; height: 150px;"><?php echo esc_attr(get_option('form_custom_css')); ?></textarea></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+add_action('admin_init', 'newsletter_subscribe_register_settings');
+
+function newsletter_subscribe_register_settings() {
+  register_setting('newsletter-subscribe-settings-group', 'provider_api_url');
+  register_setting('newsletter-subscribe-settings-group', 'provider_api_key');
+  register_setting('newsletter-subscribe-settings-group', 'form_custom_css');
+}
