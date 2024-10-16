2a86ce2d22051035c31a4036f374fb9adb3920ed
Diego Acero <diegoaceroarguelles@gmail.com>
2024-10-16 12:19:41 +0200

    Fix issues with plugin

--- js/newsletter-form.js
+++ js/newsletter-form.js
@@ -5,32 +5,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
     subscribeButton.addEventListener("click", function() {
         const email = emailInput.value.trim();
+        
+        var formData = new FormData();
+        formData.append('email', email);
+        formData.append('nonce', form_ajax_object.nonce);
+        formData.append('action', 'subscribe_newsletter');
 
         if (email) {
-            const data = {
-                action: 'subscribe_newsletter',
-                email: email,
-                nonce: form_ajax_object.nonce
-            };
+          var formData = new FormData();
+          formData.append('email', email);
+          formData.append('nonce', form_ajax_object.nonce);
+          formData.append('action', 'subscribe_newsletter');
 
-            fetch(form_ajax_object.ajax_url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            .then(response => response.json())
-            .then(responseData => {
-                if (responseData.success) {
-                    messageDiv.textContent = responseData.message;
-                } else {
-                    messageDiv.textContent = responseData.message || "Hubo un error al suscribirse.";
-                }
-            })
-            .catch(() => {
-                messageDiv.textContent = "Error al suscribirse, por favor intenta nuevamente.";
-            });
+
+          fetch(form_ajax_object.ajax_url, {
+            method: "POST",
+            body: formData
+          })
+          .then(response => response.json())
+          .then(responseData => {
+            if (responseData.success) {
+                messageDiv.textContent = responseData.data.message;
+            } else {
+                messageDiv.textContent = responseData.data.message || "Hubo un error al suscribirse.";
+            }
+          })
+          .catch(() => {
+              messageDiv.textContent = "Error al suscribirse, por favor intenta nuevamente.";
+          });
         } else {
             alert("Por favor, ingresa un email válido.");
         }
--- newsletter-subscribe-plugin.php
+++ newsletter-subscribe-plugin.php
@@ -3,7 +3,7 @@
 /**
 * Plugin Name: Newsletter Subscribe
 * Description: Plugin that allows users to create a newsletter subscription form.
-* Version: 0.1
+* Version: 0.1.1
 * Author: Diego Acero (Somosbytes)
 * Author URI: http://www.somosbytes.es
 */
@@ -19,7 +19,7 @@ function subscribe_form() {
   <div id="newsletterSubscribeContainer">
     <form id="newsletterSubscribeForm">
       <input type="email" id="newsletterSubscriptionEmail" name="email" placeholder="Email" required>
-      <button type="submit" id="newsletterSubscribeButton">Subscribe</button>
+      <button type="button" id="newsletterSubscribeButton">Subscribe</button>
     </form>
     <div id="newsletterSubscribeMessage"></div>
   </div>
@@ -28,13 +28,13 @@ function subscribe_form() {
   return ob_get_clean();
 }
 
-add_shortcode('newsletter-subscribe-form', 'subscribe_form');
+add_shortcode('NSP-form', 'subscribe_form');
 
 add_action('admin_menu', 'newsletter_subscribe_plugin_menu');
 
 function newsletter_subscribe_plugin_menu() {
   add_menu_page(
-    'Newsletter Plugins Settings',
+    'NSP plugin Settings',
     'Newsletter Settings',
     'manage_options',
     'newsletter-subscribe-settings',
@@ -79,13 +79,22 @@ function newsletter_subscribe_register_settings() {
   register_setting('newsletter-subscribe-settings-group', 'form_custom_css');
 }
 
+function nsp_enqueue_scripts() {
+    wp_enqueue_script('NSP-subscribe-script', plugins_url('/js/newsletter-form.js', __FILE__), array(), null, true);
+    wp_localize_script('NSP-subscribe-script', 'form_ajax_object', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce'    => wp_create_nonce('newsletter-subscribe-nonce')
+    ));
+}
+add_action('wp_enqueue_scripts', 'nsp_enqueue_scripts');
+
 function subscribe_newsletter() {
   check_ajax_referer('newsletter-subscribe-nonce', 'nonce');
 
-  $email = sanitiza_email($_POST['email']);
+  $email = sanitize_email($_POST['email']);
   
   if (!is_email($email)) {
-    wp_send_json_error(array('message' => 'Invalid Email');
+    wp_send_json_error(array('message' => 'Invalid Email'));
   }
 
   $api_key = get_option('provider_api_key');
@@ -93,6 +102,7 @@ function subscribe_newsletter() {
 
   if (!$api_key || !$api_url) {
     wp_send_json_error(array('message' => 'Missing provider API Key or URL'));
+  }
 
   $response = wp_remote_post($api_url, array(
     'headers' => array(
@@ -109,18 +119,18 @@ function subscribe_newsletter() {
   $body = wp_remote_retrieve_body($response);
   $result = json_decode($body, true);
 
-  if (isset($result['status']) && $result['status'] == 'sucess') {
+  $response_code = wp_remote_retrieve_response_code($response);
+  if ($response_code === 200 || $response_code === 201) {
         wp_send_json_success(array('message' => '¡Suscripción exitosa!'));
-    } else {
+  } else {
         wp_send_json_error(array('message' => 'Error al suscribirse.'));
-    }
   }
 }
 
 add_action('wp_ajax_subscribe_newsletter', 'subscribe_newsletter');
 add_action('wp_ajax_nopriv_subscribe_newsletter', 'subscribe_newsletter');
 
-fucntion form_custom_css() {
+function form_custom_css() {
   $custom_css = get_option('form_custom_css');
 
   if (!empty($custom_css)) {
